53580a190704b348510c3266fe0c898550f72026
Akash Singh <133749800+akashsingh000@users.noreply.github.com>
2025-04-14 19:10:49 +0530

    service worker remove

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -3,14 +3,4 @@ import { hydrateRoot } from 'react-dom/client';
 import App from './app';
 import { BrowserRouter } from 'react-router-dom';
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/custom-sw.js').then(reg => {
-            console.log("✅ Service Worker registered:", reg);
-        }).catch(err => {
-            console.error("❌ Service Worker registration failed:", err);
-        });
-    });
-}
-
 hydrateRoot(document.getElementById('app'), <BrowserRouter><App /></BrowserRouter>);
